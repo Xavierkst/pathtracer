87bb275a6f4ba25701ffefc2d26b40f268d94028
Xavier T``` <xavier-tan@hotmail.sg>
2021-04-04 00:37:28 -0700

    Added most parsing, remaining transforms

--- OptiXRenderer/Scene.h
+++ OptiXRenderer/Scene.h
@@ -16,18 +16,26 @@ struct Scene
     std::string integratorName;
 
     std::vector<optix::float3> vertices;
+    std::vector<optix::float3> normals;
+    // a vector of vertex + normal pairs
+    // first == vertex, second == normal
+    std::vector<std::pair<optix::float3, optix::float3>> vertexNormals;
 
     std::vector<Triangle> triangles;
     std::vector<Sphere> spheres;
+    unsigned int maxverts;
+    unsigned int maxvertnorms;
 
     std::vector<DirectionalLight> dlights;
     std::vector<PointLight> plights;
 
     // TODO: add other variables that you need here
     Camera camera;
+    unsigned int depth;
 
     Scene()
     {
+        depth = 5;
         outputFilename = "raytrace.png";
         integratorName = "raytracer";
     }
--- OptiXRenderer/SceneLoader.cpp
+++ OptiXRenderer/SceneLoader.cpp
@@ -49,6 +49,16 @@ std::shared_ptr<Scene> SceneLoader::load(std::string sceneFilename)
 
     std::string str, cmd;
 
+    // save material values to assign to each geometry
+    optix::float3 diffuse = optix::make_float3(.0f);
+    optix::float3 ambient = optix::make_float3(0.2f);
+    optix::float3 specular = optix::make_float3(.0f);
+    optix::float3 emission = optix::make_float3(.0f);
+    float shininess = .0f;
+
+    // default attenuation values to save:
+    Attenuation atten = Attenuation();
+
     // Read a line in the scene file in each iteration
     while (std::getline(in, str))
     {
@@ -79,6 +89,112 @@ std::shared_ptr<Scene> SceneLoader::load(std::string sceneFilename)
         }
         // TODO: use the examples above to handle other commands
 
+        else if ( cmd == "maxdepth" && readValues(s, 1, fvalues)) {
+            scene->depth = (unsigned int)fvalues[0]; 
+        } 
+
+        else if ( cmd == "camera" && readValues(s, 10, fvalues)) {
+            scene->camera = Camera(optix::make_float3(fvalues[0], fvalues[1], fvalues[2]), 
+                    optix::make_float3(fvalues[3], fvalues[4], fvalues[5]), 
+                    optix::make_float3(fvalues[6], fvalues[7], fvalues[8]),
+                    fvalues[9]);  
+        } 
+        
+        else if (cmd == "diffuse" && readValues(s, 3, fvalues)) {
+            diffuse = optix::make_float3(fvalues[0], fvalues[1], fvalues[2]);
+        }
+
+        else if (cmd == "ambient" && readValues(s, 3, fvalues)) {
+            ambient = optix::make_float3(fvalues[0], fvalues[1], fvalues[2]);
+        }
+
+        else if (cmd == "specular" && readValues(s, 3, fvalues)) {
+            specular = optix::make_float3(fvalues[0], fvalues[1], fvalues[2]);
+        }
+
+        else if (cmd == "emission" && readValues(s, 3, fvalues)) {
+            emission = optix::make_float3(fvalues[0], fvalues[1], fvalues[2]);
+        }
+
+        else if (cmd == "shininess" && readValues(s, 1, fvalues)) {
+            shininess = fvalues[0];
+        }
+
+        else if (cmd == "attenuation" && readValues(s, 3, fvalues)) {
+            atten.constant = fvalues[0];
+            atten.linear = fvalues[1];
+            atten.quadratic = fvalues[2];
+        }
+
+        else if (cmd == "directional" && readValues(s, 6, fvalues)) {
+           scene->dlights.push_back(DirectionalLight(
+               optix::make_float3(fvalues[0], fvalues[1], fvalues[2]), 
+               optix::make_float3(fvalues[3], fvalues[4], fvalues[5]), 
+               atten));
+        }
+
+        else if (cmd == "point" && readValues(s, 6, fvalues)) {
+           scene->plights.push_back(PointLight(
+               optix::make_float3(fvalues[0], fvalues[1], fvalues[2]), 
+               optix::make_float3(fvalues[3], fvalues[4], fvalues[5]), 
+               atten));
+        }
+
+        else if (cmd == "maxverts" && readValues(s, 1, fvalues)) {
+           scene->maxverts = (unsigned int)fvalues[0]; 
+        }
+        
+        else if (cmd == "maxvertnorms" && readValues(s, 1, fvalues)) {
+           scene->maxvertnorms = (unsigned int)fvalues[0]; 
+        }
+
+        // next is Geometry, and Transformations
+        else if (cmd == "sphere" && readValues(s, 4, fvalues)) {
+           scene->spheres.push_back(
+               Sphere(optix::make_float3(
+                   fvalues[0], fvalues[1], fvalues[2]), fvalues[3], 
+                   ambient, shininess, diffuse, specular, emission));
+        }
+
+        else if (cmd == "vertex" && readValues(s, 3, fvalues)) {
+            scene->vertices.push_back(
+                optix::make_float3(fvalues[0], fvalues[1], fvalues[2]));
+        }
+
+        else if (cmd == "vertexnormal" && readValues(s, 6, fvalues)) {
+            // scene->vertexNormals.push_back(std::make_pair(
+            //     optix::make_float3(fvalues[0], fvalues[1], fvalues[2]), 
+            //     optix::make_float3(fvalues[3], fvalues[4], fvalues[5])));
+            scene->vertices.push_back(
+                optix::make_float3(fvalues[0], fvalues[1], fvalues[2]));
+            scene->normals.push_back(
+                optix::make_float3(fvalues[3], fvalues[4], fvalues[5]));
+        }
+
+        else if (cmd == "tri" && readValues(s, 3, ivalues)) {
+            scene->triangles.push_back(Triangle(
+                scene->vertices[ivalues[0]], 
+                scene->vertices[ivalues[1]], 
+                scene->vertices[ivalues[2]], 
+                ambient, shininess, diffuse, 
+                specular, emission)); 
+        }
+
+         else if (cmd == "trinormal" && readValues(s, 3, ivalues)) {
+            scene->triangles.push_back(Triangle(
+                // load in vertices
+                scene->vertices[ivalues[0]], 
+                scene->vertices[ivalues[1]], 
+                scene->vertices[ivalues[2]], 
+                // load in normals corresp to vertices
+                scene->normals[ivalues[0]], 
+                scene->normals[ivalues[1]], 
+                scene->normals[ivalues[2]], 
+                ambient, shininess, diffuse, 
+                specular, emission)); 
+        }
+
+       
     }
 
     in.close();
