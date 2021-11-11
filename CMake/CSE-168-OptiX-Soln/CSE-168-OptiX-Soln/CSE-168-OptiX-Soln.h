6ffc5be1960bc378e262d4c964294a0901591bff
Xavier Tan <xavier-tan@hotmail.sg>
2021-11-10 23:34:40 -0800

    remove

--- CMake/CSE-168-OptiX-Soln/CSE-168-OptiX-Soln/CSE-168-OptiX-Soln.h
+++
@@ -1,8 +0,0 @@
-﻿// CSE-168-OptiX-Soln.h : Include file for standard system include files,
-// or project specific include files.
-
-#pragma once
-
-#include <iostream>
-
-// TODO: Reference additional headers your program requires here.
